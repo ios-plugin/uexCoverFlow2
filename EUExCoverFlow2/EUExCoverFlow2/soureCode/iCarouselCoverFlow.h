52061d39c786b38ed0fdc3798a6a117e017ca1f4
wzy <zhengyu.wang@3g2win.com>
2016-08-02 15:42:49 +0800

    升级4.0，去掉Id

---
+++ EUExCoverFlow2/EUExCoverFlow2/soureCode/iCarouselCoverFlow.h
@@ -0,0 +1,13 @@
+//
+//  iCarouselCoverFlow.h
+//  EUExCoverFlow2
+//
+//  Created by wang on 16/8/1.
+//  Copyright © 2016年 xll. All rights reserved.
+//
+
+#import "iCarousel.h"
+
+@interface iCarouselCoverFlow : iCarousel
+@property(nonatomic,strong) NSString *strId;
+@end
---
+++ EUExCoverFlow2/EUExCoverFlow2/soureCode/iCarouselCoverFlow.m
@@ -0,0 +1,15 @@
+//
+//  iCarouselCoverFlow.m
+//  EUExCoverFlow2
+//
+//  Created by wang on 16/8/1.
+//  Copyright © 2016年 xll. All rights reserved.
+//
+
+#import "iCarouselCoverFlow.h"
+
+@implementation iCarouselCoverFlow
+- (BOOL)isEqual:(iCarouselCoverFlow *)object {
+    return [self.strId isEqual:object.strId];
+}
+@end
